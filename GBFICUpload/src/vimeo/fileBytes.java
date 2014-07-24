4adf027c039f1d3fabf95d38175cb27f3f239ebe
Justin Parton <Justin.Parton@gmail.com>
2014-07-24 16:40:05 -0500

    Update fileBytes.java

--- GBFICUpload/src/vimeo/fileBytes.java
+++ GBFICUpload/src/vimeo/fileBytes.java
@@ -1,27 +1,61 @@
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.Arrays;
 
-public class fileBytes {
+public class uploadVideo {
+	private boolean isDebug;
+	private String fileLocation, url, vTitle;
 
-	public static void main(String[] args) throws FileNotFoundException,
-			IOException {
-		File testUp = new File("C:\\Users\\a495\\Desktop\\11.zip");
+	public uploadVideo(boolean isDebugOn, int bufferSize, String file,
+			String siteUrl, String sTitle) {
+
+		isDebug = isDebugOn;
+		bufferSize = 2097152;// ;
+		fileLocation = file;
+		url = siteUrl;
+		vTitle = sTitle;
+	}
+
+	public String uploadNow(String[] args) throws Exception {
+
+		File testUp = new File(fileLocation);
 		FileInputStream is = new FileInputStream(testUp);
-		int bufferSize = 2097152;// 2097152;
+		int bufferSize = 2097152;// ;
 		long contentLength = testUp.length();
+		consoleInfo(contentLength, bufferSize, isDebug);
 		byte[] bytesPortion = new byte[bufferSize];
-		System.out.println("File: " + contentLength);
 		int i = 1;
-		while (is.read(bytesPortion, 0, bufferSize) != -1) {
 
+		consoleDetails("------File Contents-------------", isDebug);
+
+		while (is.read(bytesPortion, 0, bufferSize) != -1) {
 			byte[] newByteChunk = Arrays.copyOfRange(bytesPortion, 0,
 					bufferSize);
-			System.out.println("File " + i + ": " + newByteChunk);
-			i++;
+
+			consoleDetails("File " + (i++) + ": " + newByteChunk, isDebug);
+
 		}
 		is.close();
+
+		return null;
+
+	}
+
+	public void consoleInfo(long contentLength, int bufferSize,
+			boolean isConsoleOn) {
+		if (isConsoleOn) {
+			System.out.println("--------Debug: Parameters--------");
+			System.out.println("File Size: " + contentLength);
+			System.out.println("File  loc: " + fileLocation);
+			System.out.println("Buffer   : " + bufferSize);
+			System.out.println("Url      : " + url);
+			System.out.println("Sermon   : " + vTitle);
+		}
+	}
+
+	public static void consoleDetails(String value, boolean isConsoleOn) {
+		if (isConsoleOn) {
+			System.out.println(value);
+		}
 	}
 }
